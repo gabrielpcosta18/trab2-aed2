e49492486b5c59dc529f0f46ab0b74e301d57562
Rúben Belém <rubenbelem@gmail.com>
2016-08-11 15:33:38 -0400

    Termo criado
    Iniciada programação das funções/TAD de Termo.

--- src/termo.c
+++ src/termo.c
@@ -1,1 +1,16 @@
+#include "termo.h"
 
+int n_containing(TTermo *t) {
+    TListaEncadeada *l = t->dado->ocorrencias;
+    return l->tamanho(l);
+}
+
+static short comparar_termo_pag(TTermo *t1, TTermo *t2) {
+    TPreproc *p1 = t1->dado, *p2 = t2->dado;
+    int pg1 = p1->p
+}
+
+double tf(TTermo *t, int pag) {
+    TListaEncadeada *l = t->dado->ocorrencias;
+
+}
--- src/termo.h
+++ src/termo.h
@@ -1,16 +1,30 @@
+#include "stringaed.h"
+#include "listaencadeada.h"
+
 typedef struct termo TTermo;
 typedef struct preproc TPreproc;
+typedef struct dado TDadoTermo;
 
+typedef short (*TComparaTermo)(TTermo*, TTermo*);
+typedef void (*TImprimirTermo)(TTermo*);
+typedef int (*TRecuperarChaveTermo)(TTermo*);
 
+double tf(TTermo *t, int pag);
+int n_containing(TTermo *t);
 
 struct preproc {
     int pag;
-    int contagem;
+    int tf;
     double tf_idf;
 };
 
-struct termo {
+struct dado {
     TStringAED *palavra;
     TListaEncadeada *ocorrencias;
+};
+
+struct termo {
+    TDadoTermo *dado;
+
 };
 
